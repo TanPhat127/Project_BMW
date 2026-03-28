8e3ec41e700c20997a9e525121c568cad0da7409
TanPhat127 <127phantanphat@gmail.com>
2026-03-29 06:52:03 +0700

    Initial .NET project setup

---
+++ Project_BMW/OAuth2_OIDC_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OAuth2_OIDC_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
